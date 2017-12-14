9dc3146b4f7ac4327a543c9a1269f7430798f998
thegoldenbeetle <thegoldenbeetle@yandex.ru>
2017-12-15 00:32:25 +0300

    Add color correction

--- Conversion.py
+++ Conversion.py
@@ -1,6 +1,6 @@
 from skimage.io import imread, imsave, imshow
 from skimage import img_as_float, img_as_ubyte
-from numpy import dstack, roll
+from numpy import dstack, roll, mean, clip
 from sys import argv
 
 def rolling(img, img_g):
@@ -36,15 +36,28 @@ def get_parameters_of_image(img):
 	height = img.shape[0]
 	if (height % 3 != 0):
 		img = img[0: height - height % 3, :]
-	height = img.shape[0]
-	width = img.shape[1]
+	height, width = img.shape[0], img.shape[1]
 	frame = int(0.1 * width)
 	return height, width, frame
 	
+def color_correction(img):
+	R, G, B = img.mean(axis=(0,1))
+	average = (R + G + B) / 3
+	rw, rg, rb = R / average, G / average, B / average
+	img_out = clip((img / [rw, rg, rb]), 0 , 255)
+	return img_out.astype('uint8')
 
-img = img_as_float(imread(argv[1]))
-height, width, frame = get_parameters_of_image(img);
-g, b, r = get_gbr(img, height, width, frame)
-b = treatment(b, g)
-r = treatment(r, g)
-imsave('out' + argv[1][1] + '.png', dstack((r, g, b)))
+def conversion(img):
+	img = img_as_float(img)
+	height, width, frame = get_parameters_of_image(img);
+	g, b, r = get_gbr(img, height, width, frame)
+	b = treatment(b, g)
+	r = treatment(r, g)
+	img_out = dstack((r, g, b))
+	img_out = color_correction(img_as_ubyte(img_out))
+	return img_out
+	
+for i in argv[1:]:
+	img = imread(i)
+	img_out = conversion(img)
+	imsave('out' + i[1] + '.png', img_out)
